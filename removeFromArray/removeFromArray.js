4ada1beef0a1eab0ede12c1d4c93c2d71590e36b
Mark Bailey <markbailey0356@gmail.com>
2018-09-04 20:23:07 +0930

    Complete removeFromArray exercise

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...values) {
+  for (let valuesIndex = 0; valuesIndex < values.length; valuesIndex++) {
+    let foundIndex = array.indexOf(values[valuesIndex])
+    if (~foundIndex)
+      array.splice(foundIndex,1);
+  }
+  return array;
 }
 
 module.exports = removeFromArray
