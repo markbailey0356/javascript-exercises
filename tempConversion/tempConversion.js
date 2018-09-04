efcd62f53085eaf6689fde8cc996d9129957ab2f
Mark Bailey <markbailey0356@gmail.com>
2018-09-04 21:00:09 +0930

    Complete tempConversion exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let celcius = (fahrenheit-32) * 5/9;
+  return +celcius.toFixed(1);
 }
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  let fahrenheit = celcius * 9/5 + 32;
+  return +fahrenheit.toFixed(1);
 }
 
 module.exports = {
