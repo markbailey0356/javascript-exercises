4ea9515cbfdb3a585d27d76ce5b6557d6a0d2afe
Mark Bailey <markbailey0356@gmail.com>
2018-09-04 20:34:58 +0930

    Complete sumAll exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+  let result = 0;
+  // ensure a & b are positive numbers
+  if (typeof a != "number" || typeof b != "number" || a < 0 || b < 0) {
+    return "ERROR";
+  }
+  // swap a and b to ensure b >= a
+  if (b < a) {
+    let temp = a;
+    a = b;
+    b = temp;
+  }
+  for (let i = a; i <= b; i++) {
+    result += i;
+  }
+  return result;
 }
 
 module.exports = sumAll
