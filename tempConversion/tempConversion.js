b5767ef48d4b2fa7c181dfdce95fc81ddfa7e99d
Mark Bailey <markbailey0356@gmail.com>
2018-09-04 21:08:45 +0930

    Change rounding mechanism in tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const ftoc = function(fahrenheit) {
   let celcius = (fahrenheit-32) * 5/9;
-  return +celcius.toFixed(1);
+  return Math.round(celcius * 10) / 10;
 }
 
 const ctof = function(celcius) {
   let fahrenheit = celcius * 9/5 + 32;
-  return +fahrenheit.toFixed(1);
+  return Math.round(fahrenheit * 10) / 10;
 }
 
 module.exports = {
