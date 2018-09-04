9cde5eeeb2417808fff5317e9ed8fa269afefe08
Mark Bailey <markbailey0356@gmail.com>
2018-09-04 16:56:14 +0930

    Complete repeatString exercise

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, numOfRepetitions) {
+  if (numOfRepetitions < 0) return "ERROR";
+  let returnValue = "";
+  for (let i = 0; i < numOfRepetitions; i++) {
+    returnValue += stringToRepeat;
+  }
+  return returnValue;
 }
 
 module.exports = repeatString
