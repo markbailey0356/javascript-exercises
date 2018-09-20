f0fb38aca0572eb88857e257ed7c83f4e69fbb70
Mark Bailey <markbailey0356@gmail.com>
2018-09-20 22:55:17 +0930

    Complete snakeCase exercise

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,10 @@
-const snakeCase = function() {
-
+const snakeCase = function(str) {
+  let wordMatches = str.match(/\w+/g);
+  if (wordMatches.length > 1) {
+    return wordMatches.join("_").toLowerCase();
+  } else {
+    return str.match(/[A-Z]?[a-z]+/g).join("_").toLowerCase();
+  }
 }
 
 module.exports = snakeCase
