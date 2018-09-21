30211500c6999332b907c08ef9698e86321aaa6c
Mark Bailey <markbailey0356@gmail.com>
2018-09-21 16:58:41 +0930

    Complete pigLatin exercise

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,7 +1,7 @@
-function translate() {
-	// body...
+function translate(str) {
+	const translateWord = str => str.replace(/^([^aeiouq]*(?:qu)?)(.*)/, "$2$1ay");
+	return str.split(" ").map(translateWord).join(" ")
 }
-
 
 module.exports = {
 	translate
