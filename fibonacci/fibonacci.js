4f489bf2f4f4eb5f9928074afbb108e140f7224e
Mark Bailey <markbailey0356@gmail.com>
2018-09-21 14:12:59 +0930

    Complete fibonacci exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+// memoized recursive algorithm
+const fibonacci = function(n) {
+  if (n <= 0) return "OOPS"
+  let memo = {1: 1, 2: 1};
+  const innerFib = function(n) {
+    if (n in memo) return memo[n]
+    return memo[n] = innerFib(n-2) + innerFib(n-1)
+  }
+  return innerFib(n);
 }
 
 module.exports = fibonacci
