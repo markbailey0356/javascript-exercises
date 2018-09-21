14caea8cd56f12212e5c6c3453a40f8080ae34ca
Mark Bailey <markbailey0356@gmail.com>
2018-09-21 14:15:06 +0930

    Change name of inner fibanacci function

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -2,11 +2,11 @@
 const fibonacci = function(n) {
   if (n <= 0) return "OOPS"
   let memo = {1: 1, 2: 1};
-  const innerFib = function(n) {
+  const $fibonacci = function(n) {
     if (n in memo) return memo[n]
-    return memo[n] = innerFib(n-2) + innerFib(n-1)
+    return memo[n] = $fibonacci(n-2) + $fibonacci(n-1)
   }
-  return innerFib(n);
+  return $fibonacci(n);
 }
 
 module.exports = fibonacci
